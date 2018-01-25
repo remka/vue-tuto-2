bc5c626d102d5966973af7d13ecc7a24c2eb1290
remka <remka@users.noreply.github.com>
2018-01-25 17:47:23 +0900

    Adding router-link

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -2,7 +2,9 @@
   <div class="thread">
     <div>
       <p>
-        <a :href="`/thread/${thread['.key']}`">{{thread.title}}</a>
+        <router-link :to="`/thread/${thread['.key']}`">
+          {{thread.title}}
+        </router-link>
       </p>
       <p class="text-faded text-xsmall">
         By <a href="#">{{user.name}}</a>, {{thread.publishedAt}}.
